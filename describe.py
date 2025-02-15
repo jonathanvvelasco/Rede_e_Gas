1b6f79a14908b1b9c227d2f02c4bc78681a0513e
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2025-02-15 14:36:11 -0300

    Expansão Norte
    - Acrescenta função de expansão histórica
    - Atualiza valores de expansão no Norte
    
    Próximo
    - Atualizar valores de expansão no NE, Sul e SE.

--- describe.py
+++ describe.py
@@ -109,7 +109,7 @@ def growth__tecnologies(make_df,scenario,local,model_horizon):
     return scenario
 
 
-def historic__generation(make_df,scenario,grid_efficiency,local,history,capacity_factor):
+def historic__generation(make_df,scenario,local,history):
     # Describe the energy generation historic.
 
     # 1- Insert the participation fraction of each technology in the generation (base year).
@@ -211,20 +211,108 @@ def historic__generation(make_df,scenario,grid_efficiency,local,history,capacity
             scenario.add_par("historical_activity", df)
 
         # 4- Define the historical expansion as (1/10) of the installed capacity in the base year.
-        for tec in old_activity:
-            value = old_activity[tec] / (1 * 10 * capacity_factor[tec])
-            df = make_df(
-                "historical_new_capacity",
-                node_loc=local,
-                year_vtg=history,
-                unit="GW",
-                technology=tec,
-                value=value,
-            )
-            scenario.add_par("historical_new_capacity", df)
+        # for tec in old_activity:
+        #     value = old_activity[tec] / (1 * 10 * capacity_factor[tec])
+        #     df = make_df(
+        #         "historical_new_capacity",
+        #         node_loc=local,
+        #         year_vtg=history,
+        #         unit="GW",
+        #         technology=tec,
+        #         value=value,
+        #     )
+        #     scenario.add_par("historical_new_capacity", df)
 
     return scenario, historic_demand_N, historic_demand_NE, historic_demand_S, historic_demand_SW, historic_act_N, historic_act_NE, historic_act_S, historic_act_SW
 
+def historic__expansion(make_df,scenario,local,history):
+    # Describe the historical expansion.
+
+    # Generation historical new capacity (GW)
+                         #hydro      #oil   #nuclear #coal  #biomass #wind    #gas    #pch   #PV
+    historic_new_cap_N  =  [[8.17,  0.01,   0.00,   0.02,   0.02,   0,      0.12,   0.23,   0       ],
+                            [2.00,  0.19,   0.00,   0.26,   0.26,   0,      1.79,   0.0367, 0       ],
+                            [9.47,  0.07,   0.00,   0.09,   0.09,   0.43,   0.62,   0.1432, 0.0028  ]]
+    historic_new_cap_NE =  [[10.8,   0.0036, 0,      0,      0.0081, 0.124,  0.8024, 2.42,   0       ],
+                            [0,      1.2889, 0,      0.9054, 0.1427, 2.0541, 1.382,  0.1600, 0.0016  ],
+                            [0,      0.1233, 0,      0.3185, 0.2924, 5.568,  0.7977, 0.2306, 0.4562  ]]
+    historic_new_cap_SW =  [[29.30,  0.3228, 1.658,  0,      0.3441, 0,      2.0201, 1.64,   0       ],
+                            [24.379, 0.4217, 1.6903, 0.0021, 2.2254, 0.0087, 5.193,  1.3644, 0.00004 ],
+                            [28.85,  0.0607, 1.5998, 0.0047, 3.2374, 0.0062, 2.317,  1.6146, 0.0008  ]]
+    historic_new_cap_S  =  [[8.2040, 0,      0,      0,      0.0005, 0.0412, 0.1619, 0.459,  0       ],
+                            [10.1951,0.144,  0,      0.8668, 0.1348, 0.4376, 0.3921, 0.5706, 0.00004 ],
+                            [4.511,  0.1272, 0,      0.7941, 0.3533, 0.7382, 0.1562, 0.25244,0.2204  ]]
+
+    for j in range(len(history)):
+
+
+        # 2- Define the generated energy values for each technology in the base year.
+        
+        if local == 'N':
+            old_expansion = {
+                "large_hydroelectric_" + local + "_ppl": historic_new_cap_N[j][0],
+                "oil_" + local + "_ppl": historic_new_cap_N[j][1],
+                "nuclear_g_" + local + "_ppl": historic_new_cap_N[j][2],
+                "national_coal_" + local + "_ppl": historic_new_cap_N[j][3],
+                "biomass_retrofit_" + local + "_ppl": historic_new_cap_N[j][4],
+                "onshore_wind_" + local + "_ppl": historic_new_cap_N[j][5],
+                "GN_open_cycle_" + local + "_ppl": historic_new_cap_N[j][6],
+                "pch_" + local + "_ppl": historic_new_cap_N[j][7],
+                "solar_photovoltaic_" + local + "_ppl": historic_new_cap_N[j][8],
+            }
+        
+        if local == 'NE':
+            old_expansion = {
+                "large_hydroelectric_" + local + "_ppl": historic_new_cap_NE[j][0],
+                "oil_" + local + "_ppl": historic_new_cap_NE[j][1],
+                "nuclear_g_" + local + "_ppl": historic_new_cap_NE[j][2],
+                "national_coal_" + local + "_ppl": historic_new_cap_NE[j][3],
+                "biomass_retrofit_" + local + "_ppl": historic_new_cap_NE[j][4],
+                "onshore_wind_" + local + "_ppl": historic_new_cap_NE[j][5],
+                "GN_open_cycle_" + local + "_ppl": historic_new_cap_NE[j][6],
+                "pch_" + local + "_ppl": historic_new_cap_NE[j][7],
+                "solar_photovoltaic_" + local + "_ppl": historic_new_cap_NE[j][8],
+            }
+        
+        if local == 'SE/CW':
+            old_expansion = {
+                "large_hydroelectric_" + local + "_ppl": historic_new_cap_SW[j][0],
+                "oil_" + local + "_ppl": historic_new_cap_SW[j][1],
+                "nuclear_g_" + local + "_ppl": historic_new_cap_SW[j][2],
+                "national_coal_" + local + "_ppl": historic_new_cap_SW[j][3],
+                "biomass_retrofit_" + local + "_ppl": historic_new_cap_SW[j][4],
+                "onshore_wind_" + local + "_ppl": historic_new_cap_SW[j][5],
+                "GN_open_cycle_" + local + "_ppl": historic_new_cap_SW[j][6],
+                "pch_" + local + "_ppl": historic_new_cap_SW[j][7],
+                "solar_photovoltaic_" + local + "_ppl": historic_new_cap_SW[j][8],
+            }
+        
+        if local == 'S':
+            old_expansion = {
+                "large_hydroelectric_" + local + "_ppl": historic_new_cap_S[j][0],
+                "oil_" + local + "_ppl": historic_new_cap_S[j][1],
+                "nuclear_g_" + local + "_ppl": historic_new_cap_S[j][2],
+                "national_coal_" + local + "_ppl": historic_new_cap_S[j][3],
+                "biomass_retrofit_" + local + "_ppl": historic_new_cap_S[j][4],
+                "onshore_wind_" + local + "_ppl": historic_new_cap_S[j][5],
+                "GN_open_cycle_" + local + "_ppl": historic_new_cap_S[j][6],
+                "pch_" + local + "_ppl": historic_new_cap_S[j][7],
+                "solar_photovoltaic_" + local + "_ppl": historic_new_cap_S[j][8],
+            }
+
+        # 3- Add values to the parameter "historical_new_capacity"
+        for tec, val in old_expansion.items():
+                df = make_df(
+                "historical_new_capacity",
+                node_loc=local,
+                year_vtg=history[j],
+                unit="GW",
+                technology=tec,
+                value=val,
+            )
+        scenario.add_par("historical_new_capacity", df)
+
+    return scenario
 
 def inv_costs(make_df,scenario,local,model_horizon):
     # Describe the investment costs of the technologies.
@@ -235,7 +323,7 @@ def inv_costs(make_df,scenario,local,model_horizon):
         "pch_" + local + "_ppl": 2600,
         "nuclear_g_" + local + "_ppl":3500,
         "biogas_" + local + "_ppl":2400,
-        "solar_photovoltaic_" + local + "_ppl":5900,
+        "solar_photovoltaic_" + local + "_ppl":1100,
         "solar_csp_" + local + "_ppl":4800,
         "onshore_wind_" + local + "_ppl":2500,
         "offshore_wind_" + local + "_ppl":3500,
