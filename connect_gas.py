145574a35a196b0d409650b9ca085ead69ae5e9b
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2025-03-20 18:41:52 -0300

    Corrige nome
    de bulb para boiler

--- connect_gas.py
+++ connect_gas.py
@@ -7,9 +7,9 @@ def technologies(scenario,base_input,base_output):
 
     # Boiler (Secondary -> Useful)
     boiler_out    = base_output.assign(technology="boiler", commodity="gas_households", level="useful", value=1.0)
-    bulb_in     = base_input.assign(technology="boiler", commodity="gas_distributed", level="secondary", value=1.0)
+    boiler_in     = base_input.assign(technology="boiler", commodity="gas_distributed", level="secondary", value=1.0)
     scenario.add_par("output", boiler_out)
-    scenario.add_par("input", bulb_in)
+    scenario.add_par("input", boiler_in)
 
     # Gas Pipelines (...-> Secondary)
     pipe_out    = base_output.assign(technology="pipelines", commodity="gas_distributed", level="secondary", value=1.0)
