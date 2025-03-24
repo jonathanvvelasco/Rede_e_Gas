99971e1931a7120dd3cbfe88796479fcd099af8a
ricsnunes <ricardoo.sergio@gmail.com>
2025-03-24 17:49:15 -0300

    Adição de tecnologias.
    Adição de Gas Onshore e Gas Offshore.

--- connect_gas.py
+++ connect_gas.py
@@ -25,5 +25,18 @@ def technologies(scenario,base_input,base_output, local):
         gasbol_out    = base_output.assign(technology="GASBOL", commodity="gas_bol", level="primary", value=1.0)
         scenario.add_par("output", gasbol_out)
 
+    #UPGN (...-> Primary)
+    upgn_out    = base_output.assign(technology="UPGN", commodity="gas_underground", level="primary", value=1.0)
+    scenario.add_par("output", upgn_out)
+
+    #GAS_ONSHORE (...-> Primary)
+    onshore_out    = base_output.assign(technology="Gas_Onshore", commodity="gas_underground", level="primary", value=1.0)
+    scenario.add_par("output", onshore_out)
+
+
+    #GAS_OFFSHORE (...-> Primary)
+    if local != "N":
+        offshore_out    = base_output.assign(technology="Gas_Offshore", commodity="gas_underground", level="primary", value=1.0)
+        scenario.add_par("output", offshore_out)
 
     return scenario
